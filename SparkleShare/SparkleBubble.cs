9e094516bcaa1fb669c3f867605ea3671f9f0d81
Philipp Gildein <rmbl@openspeak-project.org>
2010-06-11 19:23:25 +0200

    Only add actions if the notification system permits it
    Fixes ugly dialogs in Ubuntu > 9.04. Changing the instructions in the bubbles would help the usability on such systems though.

--- SparkleShare/SparkleBubble.cs
+++ SparkleShare/SparkleBubble.cs
@@ -29,6 +29,12 @@ namespace SparkleShare {
 				AttachToStatusIcon (SparkleUI.NotificationIcon);
 		}
 
+		new public void AddAction (string action, string label, ActionHandler handler) {
+				if (System.Array.IndexOf (Notifications.Global.Capabilities, "actions") > -1) {
+					base.AddAction(action, label, handler);
+				}
+		}
+
 	}
 
 }
