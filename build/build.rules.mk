0a2ea7ebb80d7bba44fe221fc7a4592260aafd87
Bertrand Lorentz <bertrand.lorentz@gmail.com>
2010-06-18 23:22:56 +0200

    [build] Remove unnecessary build rules

--- build/build.rules.mk
+++ build/build.rules.mk
@@ -20,10 +20,6 @@ RESOURCES_EXPANDED = $(addprefix $(srcdir)/, $(RESOURCES))
 RESOURCES_BUILD = $(foreach resource, $(RESOURCES_EXPANDED), \
 	-resource:$(resource),$(notdir $(resource)))
 
-INSTALL_ICONS = $(top_srcdir)/build/private-icon-theme-installer "$(mkinstalldirs)" "$(INSTALL_DATA)"
-THEME_ICONS_SOURCE = $(wildcard $(srcdir)/ThemeIcons/*/*/*.png) $(wildcard $(srcdir)/ThemeIcons/scalable/*/*.svg)
-THEME_ICONS_RELATIVE = $(subst $(srcdir)/ThemeIcons/, , $(THEME_ICONS_SOURCE))
-
 ASSEMBLY_EXTENSION = $(strip $(patsubst library, dll, $(TARGET)))
 ASSEMBLY_FILE = $(top_builddir)/bin/$(ASSEMBLY).$(ASSEMBLY_EXTENSION)
 
@@ -48,7 +44,7 @@ OUTPUT_FILES = \
 moduledir = $(INSTALL_DIR_RESOLVED)
 module_SCRIPTS = $(OUTPUT_FILES)
 
-all: $(ASSEMBLY_FILE) theme-icons
+all: $(ASSEMBLY_FILE)
 
 run: 
 	@pushd $(top_builddir); \
@@ -69,10 +65,6 @@ $(ASSEMBLY_FILE).mdb: $(ASSEMBLY_FILE)
 
 $(ASSEMBLY_FILE): $(SOURCES_BUILD) $(RESOURCES_EXPANDED) $(DEP_LINK)
 	@mkdir -p $(top_builddir)/bin
-	@if [ ! "x$(ENABLE_RELEASE)" = "xyes" ]; then \
-		$(top_srcdir)/build/dll-map-makefile-verifier $(srcdir)/Makefile.am $(srcdir)/$(notdir $@.config) && \
-		$(MONO) $(top_builddir)/build/dll-map-verifier.exe $(srcdir)/$(notdir $@.config) -iwinmm -ilibbanshee -ilibbnpx11 -ilibc -ilibc.so.6 -iintl -ilibmtp.dll -ilibigemacintegration.dylib -iCFRelease $(SOURCES_BUILD); \
-	fi;
 	$(MCS) \
 		$(GMCS_FLAGS) \
 		$(ASSEMBLY_BUILD_FLAGS) \
@@ -88,16 +80,7 @@ $(ASSEMBLY_FILE): $(SOURCES_BUILD) $(RESOURCES_EXPANDED) $(DEP_LINK)
 		cp $(EXTRA_BUNDLE) $(top_builddir)/bin; \
 	fi;
 
-theme-icons: $(THEME_ICONS_SOURCE)
-	@$(INSTALL_ICONS) -il "$(BUILD_DATA_DIR)" "$(srcdir)" $(THEME_ICONS_RELATIVE)
-
-install-data-local: $(THEME_ICONS_SOURCE)
-	@$(INSTALL_ICONS) -i "$(DESTDIR)$(pkgdatadir)" "$(srcdir)" $(THEME_ICONS_RELATIVE)
-	
-uninstall-local: $(THEME_ICONS_SOURCE)
-	@$(INSTALL_ICONS) -u "$(DESTDIR)$(pkgdatadir)" "$(srcdir)" $(THEME_ICONS_RELATIVE)
-
-EXTRA_DIST = $(SOURCES_BUILD) $(RESOURCES_EXPANDED) $(THEME_ICONS_SOURCE)
+EXTRA_DIST = $(SOURCES_BUILD) $(RESOURCES_EXPANDED)
 
 CLEANFILES = $(OUTPUT_FILES) $(ASSEMBLY_FILE).config
 DISTCLEANFILES = *.pidb
