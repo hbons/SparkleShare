7344f2cd97e3c5fa0c6c612dbfcb09c8ef82cebc
Hylke Bons <hylkebons@gmail.com>
2010-11-21 12:33:24 +0000

    Remove double space type crime in license headers

--- SparkleShare/SparkleEntry.cs
+++ SparkleShare/SparkleEntry.cs
@@ -8,7 +8,7 @@
 //
 //   This program is distributed in the hope that it will be useful,
 //   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //   GNU General private License for more details.
 //
 //   You should have received a copy of the GNU General private License
