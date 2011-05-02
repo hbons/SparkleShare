03a5ab960d8fda52dff25491b078e91e5154f965
Hylke Bons <hylkebons@gmail.com>
2011-05-02 12:02:08 +0200

    intro: revert SparkleIntro.cs from ed5db3bcdd509d50608183f85a1767381019e38f

--- SparkleShare/SparkleIntro.cs
+++ SparkleShare/SparkleIntro.cs
@@ -645,7 +645,6 @@ namespace SparkleShare {
 					};
 
 
-
 						Button button = new Button () {
 							Sensitive = false,
 							Label = _("Finish")
@@ -671,16 +670,6 @@ namespace SparkleShare {
 				table.Attach (information,  1, 2, 1, 2);
 
 				box.PackStart (table, false, false, 0);
-
-             Progressbar bar = new Progressbar ();
-                        box.PackStart (bar, true, true, 0);
-            Timer timer = new Timer () {
-                Interval = 500
-            };
-            timer.Elapsed += delegate {
-                bar.Pulse ();
-            };
-            timer.Start ();
 
 				layout_vertical.PackStart (box, false, false, 0);
 
