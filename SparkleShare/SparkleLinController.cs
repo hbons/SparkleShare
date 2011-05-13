6684a6f515ffd915b57c10bcd84f6f9b8af5646d
Hylke Bons <hylkebons@gmail.com>
2011-05-14 00:41:53 +0100

    linux controller uihelpers window: fix whitespace, style, and remove obsolete code

--- SparkleShare/SparkleLinController.cs
+++ SparkleShare/SparkleLinController.cs
@@ -14,8 +14,7 @@
 //   You should have received a copy of the GNU General Public License
 //   along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-using Mono.Unix;
-using SparkleLib;
+
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -24,218 +23,183 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 
+using Mono.Unix;
+using SparkleLib;
+
 namespace SparkleShare {
 
-	public class SparkleLinController : SparkleController {
+    public class SparkleLinController : SparkleController {
 
-		public SparkleLinController () : base ()
-		{
+        public SparkleLinController () : base ()
+        {
 
-		}
-
-
-		// Creates a .desktop entry in autostart folder to
-		// start SparkleShare automatically at login
-		public override void EnableSystemAutostart ()
-		{
-		
-			string autostart_path = SparkleHelpers.CombineMore (SparklePaths.HomePath, ".config", "autostart");
-			string desktopfile_path = SparkleHelpers.CombineMore (autostart_path, "sparkleshare.desktop");
-
-			if (!File.Exists (desktopfile_path)) {
-
-				if (!Directory.Exists (autostart_path))
-					Directory.CreateDirectory (autostart_path);
-
-					TextWriter writer = new StreamWriter (desktopfile_path);
-					writer.WriteLine ("[Desktop Entry]\n" +
-					                  "Type=Application\n" +
-					                  "Name=SparkleShare\n" +
-					                  "Exec=sparkleshare start\n" +
-					                  "Icon=folder-sparkleshare\n" +
-					                  "Terminal=false\n" +
-					                  "X-GNOME-Autostart-enabled=true\n" +
-					                  "Categories=Network");
-					writer.Close ();
-
-					// Give the launcher the right permissions so it can be launched by the user
-					UnixFileInfo file_info = new UnixFileInfo (desktopfile_path);
-					file_info.Create (FileAccessPermissions.UserReadWriteExecute);
-
-					SparkleHelpers.DebugInfo ("Controller", "Created: " + desktopfile_path);
-
-				}
-
-		}
-		
-
-		// Installs a launcher so the user can launch SparkleShare
-		// from the Internet category if needed
-		public override void InstallLauncher ()
-		{
-		
-			string apps_path = SparkleHelpers.CombineMore (SparklePaths.HomePath, ".local", "share", "applications");
-			string desktopfile_path = SparkleHelpers.CombineMore (apps_path, "sparkleshare.desktop");
-
-			if (!File.Exists (desktopfile_path)) {
-
-				if (!Directory.Exists (apps_path))
-					Directory.CreateDirectory (apps_path);
-
-				TextWriter writer = new StreamWriter (desktopfile_path);
-				writer.WriteLine ("[Desktop Entry]\n" +
-				                  "Type=Application\n" +
-				                  "Name=SparkleShare\n" +
-				                  "Comment=Share documents\n" +
-				                  "Exec=sparkleshare start\n" +
-				                  "Icon=folder-sparkleshare\n" +
-				                  "Terminal=false\n" +
-				                  "Categories=Network;");
-				writer.Close ();
-
-				// Give the launcher the right permissions so it can be launched by the user
-				UnixFileInfo file_info = new UnixFileInfo (desktopfile_path);
-				file_info.FileAccessPermissions = FileAccessPermissions.UserReadWriteExecute;
-
-				SparkleHelpers.DebugInfo ("Controller", "Created '" + desktopfile_path + "'");
-
-			}
-
-		}
+        }
 
 
-		// Adds the SparkleShare folder to the user's
-		// list of bookmarked places
-		public override void AddToBookmarks ()
-		{
+        // Creates a .desktop entry in autostart folder to
+        // start SparkleShare automatically at login
+        public override void EnableSystemAutostart ()
+        {
+            string autostart_path = SparkleHelpers.CombineMore (SparklePaths.HomePath, ".config", "autostart");
+            string desktopfile_path = SparkleHelpers.CombineMore (autostart_path, "sparkleshare.desktop");
 
-			string bookmarks_file_path   = Path.Combine (SparklePaths.HomePath, ".gtk-bookmarks");
-			string sparkleshare_bookmark = "file://" + SparklePaths.SparklePath + " SparkleShare";
+            if (!File.Exists (desktopfile_path)) {
+                if (!Directory.Exists (autostart_path))
+                    Directory.CreateDirectory (autostart_path);
 
-			if (File.Exists (bookmarks_file_path)) {
+                TextWriter writer = new StreamWriter (desktopfile_path);
+                writer.WriteLine ("[Desktop Entry]\n" +
+                                  "Type=Application\n" +
+                                  "Name=SparkleShare\n" +
+                                  "Exec=sparkleshare start\n" +
+                                  "Icon=folder-sparkleshare\n" +
+                                  "Terminal=false\n" +
+                                  "X-GNOME-Autostart-enabled=true\n" +
+                                  "Categories=Network");
+                writer.Close ();
 
-				StreamReader reader = new StreamReader (bookmarks_file_path);
-				string bookmarks = reader.ReadToEnd ();
-				reader.Close ();
+                // Give the launcher the right permissions so it can be launched by the user
+                UnixFileInfo file_info = new UnixFileInfo (desktopfile_path);
+                file_info.Create (FileAccessPermissions.UserReadWriteExecute);
 
-				if (!bookmarks.Contains (sparkleshare_bookmark)) {
+                SparkleHelpers.DebugInfo ("Controller", "Created: " + desktopfile_path);
+            }
+        }
+        
 
-					TextWriter writer = File.AppendText (bookmarks_file_path);
-					writer.WriteLine ("file://" + SparklePaths.SparklePath + " SparkleShare");
-					writer.Close ();
+        // Installs a launcher so the user can launch SparkleShare
+        // from the Internet category if needed
+        public override void InstallLauncher ()
+        {
+            string apps_path = SparkleHelpers.CombineMore (SparklePaths.HomePath, ".local", "share", "applications");
+            string desktopfile_path = SparkleHelpers.CombineMore (apps_path, "sparkleshare.desktop");
 
-				}
+            if (!File.Exists (desktopfile_path)) {
+                if (!Directory.Exists (apps_path))
+                    Directory.CreateDirectory (apps_path);
 
-			} else {
+                TextWriter writer = new StreamWriter (desktopfile_path);
+                writer.WriteLine ("[Desktop Entry]\n" +
+                                  "Type=Application\n" +
+                                  "Name=SparkleShare\n" +
+                                  "Comment=Share documents\n" +
+                                  "Exec=sparkleshare start\n" +
+                                  "Icon=folder-sparkleshare\n" +
+                                  "Terminal=false\n" +
+                                  "Categories=Network;");
+                writer.Close ();
 
-				StreamWriter writer = new StreamWriter (bookmarks_file_path);
-				writer.WriteLine ("file://" + SparklePaths.SparklePath + " SparkleShare");
-				writer.Close ();
+                // Give the launcher the right permissions so it can be launched by the user
+                UnixFileInfo file_info = new UnixFileInfo (desktopfile_path);
+                file_info.FileAccessPermissions = FileAccessPermissions.UserReadWriteExecute;
 
-			}
+                SparkleHelpers.DebugInfo ("Controller", "Created '" + desktopfile_path + "'");
+            }
+        }
 
-		}
+
+        // Adds the SparkleShare folder to the user's
+        // list of bookmarked places
+        public override void AddToBookmarks ()
+        {
+            string bookmarks_file_path   = Path.Combine (SparklePaths.HomePath, ".gtk-bookmarks");
+            string sparkleshare_bookmark = "file://" + SparklePaths.SparklePath + " SparkleShare";
+
+            if (File.Exists (bookmarks_file_path)) {
+                StreamReader reader = new StreamReader (bookmarks_file_path);
+                string bookmarks = reader.ReadToEnd ();
+                reader.Close ();
+
+                if (!bookmarks.Contains (sparkleshare_bookmark)) {
+                    TextWriter writer = File.AppendText (bookmarks_file_path);
+                    writer.WriteLine ("file://" + SparklePaths.SparklePath + " SparkleShare");
+                    writer.Close ();
+                }
+            } else {
+                StreamWriter writer = new StreamWriter (bookmarks_file_path);
+                writer.WriteLine ("file://" + SparklePaths.SparklePath + " SparkleShare");
+                writer.Close ();
+            }
+        }
 
 
-		// Creates the SparkleShare folder in the user's home folder
-		public override bool CreateSparkleShareFolder ()
-		{
+        // Creates the SparkleShare folder in the user's home folder
+        public override bool CreateSparkleShareFolder ()
+        {
+            if (!Directory.Exists (SparklePaths.SparklePath)) {
+        
+                Directory.CreateDirectory (SparklePaths.SparklePath);
+                SparkleHelpers.DebugInfo ("Controller", "Created '" + SparklePaths.SparklePath + "'");
 
-			if (!Directory.Exists (SparklePaths.SparklePath)) {
-		
-				Directory.CreateDirectory (SparklePaths.SparklePath);
-				SparkleHelpers.DebugInfo ("Controller", "Created '" + SparklePaths.SparklePath + "'");
+                string icon_file_path = SparkleHelpers.CombineMore (Defines.DATAROOTDIR, "icons", "hicolor",
+                    "48x48", "apps", "folder-sparkleshare.png");
 
-				string icon_file_path = SparkleHelpers.CombineMore (Defines.DATAROOTDIR, "icons", "hicolor",
-					"48x48", "apps", "folder-sparkleshare.png");
+                string gvfs_command_path = SparkleHelpers.CombineMore (Path.VolumeSeparatorChar.ToString (),
+                    "usr", "bin", "gvfs-set-attribute");
 
-				string gvfs_command_path = SparkleHelpers.CombineMore (Path.VolumeSeparatorChar.ToString (),
-					"usr", "bin", "gvfs-set-attribute");
+                // Add a special icon to the SparkleShare folder
+                if (File.Exists (gvfs_command_path)) {
+                    Process process = new Process ();
 
-				// Add a special icon to the SparkleShare folder
-				if (File.Exists (gvfs_command_path)) {
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.UseShellExecute        = false;
+                    process.StartInfo.FileName               = "gvfs-set-attribute";
+                    process.StartInfo.Arguments              = SparklePaths.SparklePath + " metadata::custom-icon " +
+                                                               "file://" + icon_file_path;
+                    process.Start ();
+                }
 
-					Process process = new Process ();
+                return true;
+            }
 
-					process.StartInfo.RedirectStandardOutput = true;
-					process.StartInfo.UseShellExecute = false;
+            return false;
+        }
+        
 
-					process.StartInfo.FileName  = "gvfs-set-attribute";
-					process.StartInfo.Arguments = SparklePaths.SparklePath + " metadata::custom-icon " +
-							                      "file://" + icon_file_path;
-					process.Start ();
+        public override string EventLogHTML {
+            get {
+                string path = SparkleHelpers.CombineMore (Defines.PREFIX,
+                    "share", "sparkleshare", "html", "event-log.html");
+            
+                return String.Join (Environment.NewLine, File.ReadAllLines (path));
+            }
+        }
 
-				}
+        
+        public override string DayEntryHTML {
+            get {
+                string path = SparkleHelpers.CombineMore (Defines.PREFIX,
+                    "share", "sparkleshare", "html", "day-entry.html");
+            
+                return String.Join (Environment.NewLine, File.ReadAllLines (path));
+            }
+        }
 
-				return true;
+        
+        public override string EventEntryHTML {
+            get {
+                string path = SparkleHelpers.CombineMore (Defines.PREFIX,
+                    "share", "sparkleshare", "html", "event-entry.html");
+            
+                return String.Join (Environment.NewLine, File.ReadAllLines (path));
+            }
+        }
 
-			}
+            
+        public override void OpenSparkleShareFolder (string subfolder)
+        {
+            string open_command_path = SparkleHelpers.CombineMore (Path.VolumeSeparatorChar.ToString (),
+                "usr", "bin", "xdg-open");
 
-			return false;
+            if (!File.Exists (open_command_path))
+                return;
 
-		}
-		
+            string folder = SparkleHelpers.CombineMore (SparklePaths.SparklePath, subfolder);
 
-		public override string EventLogHTML {
-			
-			get {
-				
-				string path = SparkleHelpers.CombineMore (Defines.PREFIX,
-					"share", "sparkleshare", "html", "event-log.html");
-			
-				return String.Join (Environment.NewLine, File.ReadAllLines (path));
-
-			}
-			
-		}
-
-		
-		public override string DayEntryHTML {
-			
-			get {
-				
-				string path = SparkleHelpers.CombineMore (Defines.PREFIX,
-					"share", "sparkleshare", "html", "day-entry.html");
-			
-				return String.Join (Environment.NewLine, File.ReadAllLines (path));
-			
-			}
-			
-		}
-
-		
-		public override string EventEntryHTML {
-			
-			get {
-				
-				string path = SparkleHelpers.CombineMore (Defines.PREFIX,
-					"share", "sparkleshare", "html", "event-entry.html");
-			
-				return String.Join (Environment.NewLine, File.ReadAllLines (path));
-			
-			}
-			
-		}
-
-			
-		public override void OpenSparkleShareFolder (string subfolder)
-		{
-
-			string open_command_path = SparkleHelpers.CombineMore (Path.VolumeSeparatorChar.ToString (),
-				"usr", "bin", "xdg-open");
-
-			if (!File.Exists (open_command_path))
-				return;
-
-			string folder = SparkleHelpers.CombineMore (SparklePaths.SparklePath, subfolder);
-
-			Process process = new Process ();
-			process.StartInfo.Arguments = folder.Replace (" ", "\\ "); // Escape space-characters
-			process.StartInfo.FileName  = "xdg-open";
-			process.Start ();
-			
-		}
-
-	}
-
+            Process process = new Process ();
+            process.StartInfo.Arguments = folder.Replace (" ", "\\ "); // Escape space-characters
+            process.StartInfo.FileName  = "xdg-open";
+            process.Start ();
+        }
+    }
 }
--- SparkleShare/SparkleWindow.cs
+++ SparkleShare/SparkleWindow.cs
@@ -14,9 +14,7 @@
 //   You should have received a copy of the GNU General Public License
 //   along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-using Gtk;
-using Mono.Unix;
-using SparkleLib;
+
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -24,127 +22,113 @@ using System.IO;
 using System.Text.RegularExpressions;
 using System.Timers;
 
+using Gtk;
+using Mono.Unix;
+using SparkleLib;
+
 namespace SparkleShare {
 
-	public class SparkleWindow : Window	{
+    public class SparkleWindow : Window    {
 
-		private HBox HBox;
-		private VBox VBox;
-		private VBox Wrapper;
-		private HButtonBox Buttons;
-
-
-		public SparkleWindow () : base ("")
-		{
-
-			Title          = "SparkleShare Setup";
-			BorderWidth    = 0;
-			IconName       = "folder-sparkleshare";
-			Resizable      = false;
-			WindowPosition = WindowPosition.Center;
-
-			SetSizeRequest (680, 440);
-
-			DeleteEvent += delegate (object o, DeleteEventArgs args) {
-
-				args.RetVal = true;
-				Close ();
-
-			};
-
-			HBox = new HBox (false, 6);
-
-				VBox = new VBox (false, 0);
-
-					Wrapper = new VBox (false, 0) {
-						BorderWidth = 30
-					};
-
-					Buttons = CreateButtonBox ();
-
-				VBox.PackStart (Wrapper, true, true, 0);
-				VBox.PackStart (Buttons, false, false, 0);
-
-				EventBox box = new EventBox ();
-				Gdk.Color bg_color = new Gdk.Color ();
-				Gdk.Color.Parse ("#2e3336", ref bg_color);
-				box.ModifyBg (StateType.Normal, bg_color);
-
-					string image_path = SparkleHelpers.CombineMore (Defines.DATAROOTDIR, "sparkleshare",
-						"pixmaps", "side-splash.png");
-
-					Image side_splash = new Image (image_path) {
-						Yalign = 1
-					};
-
-				box.Add (side_splash);
-
-			HBox.PackStart (box, false, false, 0);
-			HBox.PackStart (VBox, true, true, 0);
-
-			base.Add (HBox);
-
-		}
+        private HBox HBox;
+        private VBox VBox;
+        private VBox Wrapper;
+        private HButtonBox Buttons;
 
 
-		private HButtonBox CreateButtonBox ()
-		{
+        public SparkleWindow () : base ("")
+        {
+            Title          = "SparkleShare Setup";
+            BorderWidth    = 0;
+            IconName       = "folder-sparkleshare";
+            Resizable      = false;
+            WindowPosition = WindowPosition.Center;
 
-			return new HButtonBox () {
-				BorderWidth = 12,
-				Layout      = ButtonBoxStyle.End,
-				Spacing     = 6
-			};
+            SetSizeRequest (680, 440);
 
-		}
+            DeleteEvent += delegate (object o, DeleteEventArgs args) {
+                args.RetVal = true;
+                Close ();
+            };
+
+            HBox = new HBox (false, 6);
+
+                VBox = new VBox (false, 0);
+
+                    Wrapper = new VBox (false, 0) {
+                        BorderWidth = 30
+                    };
+
+                    Buttons = CreateButtonBox ();
+
+                VBox.PackStart (Wrapper, true, true, 0);
+                VBox.PackStart (Buttons, false, false, 0);
+
+                EventBox box = new EventBox ();
+                Gdk.Color bg_color = new Gdk.Color ();
+                Gdk.Color.Parse ("#2e3336", ref bg_color);
+                box.ModifyBg (StateType.Normal, bg_color);
+
+                    string image_path = SparkleHelpers.CombineMore (Defines.DATAROOTDIR, "sparkleshare",
+                        "pixmaps", "side-splash.png");
+
+                    Image side_splash = new Image (image_path) {
+                        Yalign = 1
+                    };
+
+                box.Add (side_splash);
+
+            HBox.PackStart (box, false, false, 0);
+            HBox.PackStart (VBox, true, true, 0);
+
+            base.Add (HBox);
+        }
 
 
-		public void AddButton (Button button)
-		{
-
-			Buttons.Add (button);
-			ShowAll ();
-
-		}
-
-
-		new public void Add (Widget widget)
-		{
-
-			Wrapper.PackStart (widget, true, true, 0);
-			ShowAll ();
-
-		}
+        private HButtonBox CreateButtonBox ()
+        {
+            return new HButtonBox () {
+                BorderWidth = 12,
+                Layout      = ButtonBoxStyle.End,
+                Spacing     = 6
+            };
+        }
 
 
-		public void Reset ()
-		{
+        public void AddButton (Button button)
+        {
+            Buttons.Add (button);
+            ShowAll ();
+        }
 
-			if (Wrapper.Children.Length > 0)
-				Wrapper.Remove (Wrapper.Children [0]);
 
-			foreach (Button button in Buttons)
-				Buttons.Remove (button);
+        new public void Add (Widget widget)
+        {
+            Wrapper.PackStart (widget, true, true, 0);
+            ShowAll ();
+        }
 
-			ShowAll ();
 
-		}
-		
-		new public void ShowAll ()
-		{
-		
-			Present ();
-			base.ShowAll ();
-		
-		}
+        public void Reset ()
+        {
+            if (Wrapper.Children.Length > 0)
+                Wrapper.Remove (Wrapper.Children [0]);
 
-		public void Close ()
-		{
+            foreach (Button button in Buttons)
+                Buttons.Remove (button);
 
-			HideAll ();
+            ShowAll ();
+        }
+        
+        new public void ShowAll ()
+        {
+            Present ();
+            base.ShowAll ();
+        }
 
-		}
-
-	}
-
+        public void Close ()
+        {
+            HideAll ();
+        }
+    }
 }
