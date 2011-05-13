31e5e6ce1a4819164cb21bc2cae10b88fa76f832
Hylke Bons <hylkebons@gmail.com>
2011-05-13 01:35:19 +0100

    mac appdelegate: correct namespace name

--- SparkleShare/Mac/AppDelegate.cs
+++ SparkleShare/Mac/AppDelegate.cs
@@ -1,21 +1,31 @@
+//   SparkleShare, an instant update workflow to Git.
+//   Copyright (C) 2010  Hylke Bons <hylkebons@gmail.com>
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
 using System;
 using System.Drawing;
+
 using MonoMac.Foundation;
 using MonoMac.AppKit;
 using MonoMac.ObjCRuntime;
 
-namespace test2
+namespace SparkleShare
 {
 	public partial class AppDelegate : NSApplicationDelegate
 	{
-		
-		public AppDelegate ()
-		{
-		}
 
-		public override void FinishedLaunching (NSObject notification)
-		{
-		}
 	}
 }
-
