ba7db76a399e7d44bf5e3e1e40fc5c829340cb74
Hylke Bons <hylkebons@gmail.com>
2011-05-19 17:05:58 +0100

    save work

---
+++ SparkleLib/SparkleRepoBase.cs
@@ -0,0 +1,479 @@
+//   SparkleShare, an instant update workflow to Git.
+//   Copyright (C) 2010  Hylke Bons <hylkebons@gmail.com>
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics; // remove
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Timers;
+
+namespace SparkleLib {
+
+    public enum SyncStatus {
+        Idle,
+        SyncUp,
+        SyncDown,
+        Error
+    }
+
+
+    public abstract class SparkleRepoBase {
+
+        public readonly SparkleBackend Backend;
+        public readonly string LocalPath;
+        public readonly string Name;
+
+        protected SyncStatus status;
+        protected bool is_buffering  = false;
+        protected bool is_polling    = true;
+        protected bool server_online = true;
+
+        private Timer local_timer = new Timer () { Interval = 250 };
+        private Timer remote_timer = new Timer () { Interval = 60000 };
+        private FileSystemWatcher watcher;
+        private SparkleListenerBase listener;
+        private List <double> sizebuffer = new List<double> ();
+        private bool has_changed   = false;
+        private Object change_lock = new Object ();
+
+        public string Domain {
+            get {
+                Regex regex = new Regex (@"*://(.+)(/|:)*");
+                Match match = regex.Match (Url);
+
+                if (match.Success)
+                    return match.Groups [1].Value;
+                else
+                    return null;
+            }
+        }
+
+        public abstract string Url { get; }
+        public abstract bool AnyDifferences { get; }
+        public abstract string Identifier { get; }
+        public abstract string CurrentRevision { get; }
+        public abstract bool SyncUp ();
+        public abstract bool SyncDown ();
+
+        public virtual bool CheckForRemoteChanges () // HasRemoteChanges { get; } ?
+        {
+            return true;
+        }
+
+        public virtual List<SparkleChangeSet> GetChangeSets (int count) {
+            return null;
+        }
+
+        public virtual bool UsesNotificationCenter {
+            get {
+                return true;
+            }
+        }
+
+        public string RemoteName {
+            get {
+                return Path.GetFileNameWithoutExtension (Url);
+            }
+        }
+
+
+        public bool IsBuffering {
+            get {
+                return this.is_buffering;
+            }
+        }
+
+        public bool IsPolling {
+            get {
+                return this.is_polling;
+            }
+        }
+
+        public abstract bool HasUnsyncedChanges { get; set; }
+
+        public bool ServerOnline {
+            get {
+                return this.server_online;
+            }
+        }
+
+        public SyncStatus Status {
+            get {
+                return this.status;
+            }
+        }
+
+        public delegate void SyncStatusChangedEventHandler (SyncStatus new_status);
+        public event SyncStatusChangedEventHandler SyncStatusChanged;
+
+
+        public delegate void NewChangeSetEventHandler (SparkleChangeSet change_set, string source_path);
+        public delegate void ConflictResolvedEventHandler ();
+        public delegate void ChangesDetectedEventHandler ();
+
+        public event NewChangeSetEventHandler NewChangeSet;
+        public event ConflictResolvedEventHandler ConflictResolved;
+        public event ChangesDetectedEventHandler ChangesDetected;
+
+        protected void OnConflictResolved ()
+        {
+            if (ConflictResolved != null)
+                ConflictResolved ();
+        }
+
+        // TODO: constructor (path, url, backend)
+        public SparkleRepoBase (string path, SparkleBackend backend)
+        {
+            LocalPath = path;
+            Name      = Path.GetFileName (LocalPath);
+            Backend   = backend;
+
+            SyncStatusChanged += delegate (SyncStatus status) {
+                this.status = status;
+            };
+
+            if (CurrentRevision == null) {
+                CreateInitialChangeSet ();
+                SyncUpBase ();
+            }
+
+            // Watch the repository's folder
+            this.watcher = new FileSystemWatcher (LocalPath) {
+                IncludeSubdirectories = true,
+                EnableRaisingEvents   = true,
+                Filter                = "*"
+            };
+
+            this.watcher.Changed += new FileSystemEventHandler (OnFileActivity);
+            this.watcher.Created += new FileSystemEventHandler (OnFileActivity);
+            this.watcher.Deleted += new FileSystemEventHandler (OnFileActivity);
+            this.watcher.Renamed += new RenamedEventHandler (OnFileActivity);
+
+            CreateListener ();
+
+
+            this.local_timer.Elapsed += delegate (object o, ElapsedEventArgs args) {
+                CheckForChanges ();
+            };
+
+            this.remote_timer.Elapsed += delegate {
+                if (this.is_polling) {
+                    if (CheckForRemoteChanges ())
+                        SyncDownBase ();
+                }
+
+                if (this.is_polling && !this.listener.IsConnected)
+                    this.listener.Connect ();
+
+                if (HasUnsyncedChanges)
+                    SyncUpBase ();
+            };
+
+            this.remote_timer.Start ();
+            this.local_timer.Start ();
+
+            // Sync up everything that changed
+            // since we've been off
+            DisableWatching ();
+            if (AnyDifferences) {
+                SyncUpBase ();
+                while (HasUnsyncedChanges)
+                    SyncUpBase ();
+            }
+            EnableWatching ();
+        }
+
+
+        private void CreateListener ()
+        {
+            NotificationServerType server_type;
+            if (UsesNotificationCenter)
+                server_type = NotificationServerType.Central;
+            else
+                server_type = NotificationServerType.Own;
+            this.listener = new SparkleListenerIrc (Domain, Identifier, server_type);////////////
+
+            // Stop polling when the connection to the irc channel is succesful
+            this.listener.Connected += delegate {
+                this.is_polling = false;
+
+                // Check for changes manually one more time
+                CheckForRemoteChanges ();
+
+                // Push changes that were made since the last disconnect
+                if (HasUnsyncedChanges)
+                    SyncUpBase ();
+            };
+
+            // Start polling when the connection to the irc channel is lost
+            this.listener.Disconnected += delegate {
+                SparkleHelpers.DebugInfo ("Local", "[" + Name + "] Falling back to polling");
+                this.is_polling = true;
+            };
+
+            // Fetch changes when there is a message in the irc channel
+            this.listener.RemoteChange += delegate (string change_id) {
+                if (!change_id.Equals (CurrentRevision) && change_id.Length == 40) {
+                    if (Status != SyncStatus.SyncUp   &&
+                        Status != SyncStatus.SyncDown &&
+                        !this.is_buffering) {
+
+                        while (this.listener.ChangesQueue > 0) {
+                            SyncDownBase ();
+                            this.listener.DecrementChangesQueue ();
+                        }
+                    }
+                }
+            };
+
+            // Start listening
+            this.listener.Connect ();
+        }
+
+
+        private void CheckForChanges ()
+        {
+            lock (this.change_lock) {
+                if (this.has_changed) {
+                    if ( this.sizebuffer.Count >= 4)
+                         this.sizebuffer.RemoveAt (0);
+                        
+                    DirectoryInfo dir_info = new DirectoryInfo (LocalPath);
+                     this.sizebuffer.Add (CalculateFolderSize (dir_info));
+
+                    if ( this.sizebuffer [0].Equals (this.sizebuffer [1]) &&
+                         this.sizebuffer [1].Equals (this.sizebuffer [2]) &&
+                         this.sizebuffer [2].Equals (this.sizebuffer [3])) {
+
+                        SparkleHelpers.DebugInfo ("Local", "[" + Name + "] Changes have settled.");
+                        this.is_buffering = false;
+                        this.has_changed  = false;
+                        
+                        DisableWatching ();
+                        while (AnyDifferences)
+                            SyncUpBase ();//TODO look at algorithm
+                        EnableWatching ();
+                    }
+                }
+            }
+        }
+
+
+        // Starts a timer when something changes
+        private void OnFileActivity (object o, FileSystemEventArgs fse_args)
+        {
+            if (fse_args.Name.StartsWith (".git/"))
+                return;
+
+            WatcherChangeTypes wct = fse_args.ChangeType;
+
+            if (AnyDifferences) {
+                this.is_buffering = true;
+
+                // Only fire the event if the timer has been stopped.
+                // This prevents multiple events from being raised whilst "buffering".
+                if (!this.has_changed) {
+                    if (ChangesDetected != null)
+                        ChangesDetected ();
+                }
+
+                SparkleHelpers.DebugInfo ("Event", "[" + Name + "] " + wct.ToString () + " '" + fse_args.Name + "'");
+                SparkleHelpers.DebugInfo ("Local", "[" + Name + "] Changes found, checking if settled.");
+                
+                this.remote_timer.Stop ();
+
+                lock (this.change_lock) {
+                    this.has_changed = true;
+                }
+            }
+        }
+
+
+        public void SyncUpBase ()
+        {
+            try {
+                this.local_timer.Stop ();
+                this.remote_timer.Stop ();
+
+                SparkleHelpers.DebugInfo ("SyncUp", "[" + Name + "] Initiated");
+
+                //if (AnyDifferences) {
+                    if (SyncStatusChanged != null)
+                        SyncStatusChanged (SyncStatus.SyncUp);
+
+                if (SyncUp ()) {
+                    SparkleHelpers.DebugInfo ("SyncUp", "[" + Name + "] Done");
+
+                    HasUnsyncedChanges = false;
+
+                    if (SyncStatusChanged != null)
+                        SyncStatusChanged (SyncStatus.Idle);
+
+                    this.listener.Announce (CurrentRevision);
+                } else {
+                    SparkleHelpers.DebugInfo ("SyncUp", "[" + Name + "] Error");
+
+                    HasUnsyncedChanges = true;
+
+                    if (SyncStatusChanged != null)
+                        SyncStatusChanged (SyncStatus.Error);
+
+                    SyncDownBase ();
+                }
+                //}
+            } finally {
+                this.remote_timer.Start ();
+                this.local_timer.Start ();
+            }
+        }
+
+
+        private void SyncDownBase ()
+        {
+            SparkleHelpers.DebugInfo ("SyncDown", "[" + Name + "] Initiated");
+            this.remote_timer.Stop ();
+
+            if (SyncStatusChanged != null)
+                SyncStatusChanged (SyncStatus.SyncDown);
+
+            if (SyncDown ()) {
+                SparkleHelpers.DebugInfo ("SyncDown", "[" + Name + "] Done");
+                this.server_online = true;
+
+                if (SyncStatusChanged != null)
+                    SyncStatusChanged (SyncStatus.Idle);
+            } else {
+                SparkleHelpers.DebugInfo ("SyncDown", "[" + Name + "] Error");
+                this.server_online = false;
+
+                if (SyncStatusChanged != null)
+                    SyncStatusChanged (SyncStatus.Error);
+            }
+
+            if (SyncStatusChanged != null)
+                SyncStatusChanged (SyncStatus.Idle);
+
+            this.remote_timer.Start ();
+
+            if (NewChangeSet != null)
+                NewChangeSet (GetChangeSets (1) [0], LocalPath);
+        }
+
+
+        public void DisableWatching ()
+        {
+            this.watcher.EnableRaisingEvents = false;
+        }
+
+
+        public void EnableWatching ()
+        {
+            this.watcher.EnableRaisingEvents = true;
+        }
+
+
+        private string GetUserName () // TODO
+        {
+            SparkleGit git = new SparkleGit (LocalPath, "config --get user.name");
+            git.Start ();
+            git.WaitForExit ();
+
+            string output = git.StandardOutput.ReadToEnd ();
+            string user_name   = output.Trim ();
+
+            return user_name;
+        }
+
+
+        private string GetUserEmail ()
+        {
+            SparkleGit git = new SparkleGit (LocalPath, "config --get user.email");
+            git.Start ();
+            git.WaitForExit ();
+
+            string output = git.StandardOutput.ReadToEnd ();
+            string user_email   = output.Trim ();
+
+            return user_email;
+        }
+
+
+        // Recursively gets a folder's size in bytes
+        private double CalculateFolderSize (DirectoryInfo parent)
+        {
+            if (!System.IO.Directory.Exists (parent.ToString ()))
+                return 0;
+
+            double size = 0;
+
+            // Ignore the temporary 'rebase-apply' directory. This prevents potential
+            // crashes when files are being queried whilst the files have already been deleted.
+            if (parent.Name.Equals ("rebase-apply"))
+                return 0;
+
+            foreach (FileInfo file in parent.GetFiles()) {
+                if (!file.Exists)
+                    return 0;
+
+                size += file.Length;
+            }
+
+            foreach (DirectoryInfo directory in parent.GetDirectories())
+                size += CalculateFolderSize (directory);
+
+            return size;
+        }
+
+
+        // Create an initial change set when the
+        // user has fetched an empty remote folder
+        public virtual void CreateInitialChangeSet ()
+        {
+            string file_path = Path.Combine (LocalPath, "SparkleShare.txt");
+            TextWriter writer = new StreamWriter (file_path);
+            writer.WriteLine (":)");
+            writer.Close ();
+        }
+
+
+        public string GetConfigItem (string name)
+        {
+            if (String.Compare (name, "sparkleshare.user.name", true) == 0)
+                return GetUserName ();
+            else if (String.Compare (name, "sparkleshare.user.email", true) == 0)
+                return GetUserEmail ();
+            else
+                return null;
+        }
+
+
+        public static bool IsRepo (string path)
+        {
+            return System.IO.Directory.Exists (Path.Combine (path, ".git"));
+        }
+
+
+        // Disposes all resourses of this object
+        public void Dispose ()
+        {
+            this.remote_timer.Dispose ();
+            this.local_timer.Dispose ();
+            this.listener.Dispose ();
+        }
+    }
+}
