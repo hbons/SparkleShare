7d0c9d62621bb4217f4cb84a304198176119df21
wimh <wimh@github>
2011-06-18 23:45:30 +0200

    windows: locate texttransform.exe

--- SparkleLib/windows/transform_tt.cmd
+++ SparkleLib/windows/transform_tt.cmd
@@ -1,7 +1,14 @@
 @echo off
 
+if "%CommonProgramFiles(x86)%"=="" (set common=%CommonProgramFiles%) else (set common=%CommonProgramFiles(x86)%)
+if "%ProgramFiles(x86)%"=="" (set program=%ProgramFiles%) else (set program=%ProgramFiles(x86)%)
+
+set TextTransform="%common%\Microsoft Shared\TextTemplating\10.0\texttransform.exe"
+if not exist %TextTransform% set TextTransform="%common%\Microsoft Shared\TextTemplating\1.2\texttransform.exe"
+if not exist %TextTransform% set TextTransform="%program%\MonoDevelop\AddIns\MonoDevelop.TextTemplating\TextTransform.exe"
+
 echo running texttransform..
 cd %~dp0
 
-"%CommonProgramFiles%\Microsoft Shared\TextTemplating\1.2\texttransform.exe" -out Defines.cs Defines.tt
-"%CommonProgramFiles%\Microsoft Shared\TextTemplating\1.2\texttransform.exe" -out GlobalAssemblyInfo.cs GlobalAssemblyInfo.tt
+%TextTransform% -out Defines.cs Defines.tt
+%TextTransform% -out GlobalAssemblyInfo.cs GlobalAssemblyInfo.tt
--- SparkleShare/Windows/transform_tt.cmd
+++ SparkleShare/Windows/transform_tt.cmd
@@ -1,8 +1,15 @@
 @echo off
 
+if "%CommonProgramFiles(x86)%"=="" (set common=%CommonProgramFiles%) else (set common=%CommonProgramFiles(x86)%)
+if "%ProgramFiles(x86)%"=="" (set program=%ProgramFiles%) else (set program=%ProgramFiles(x86)%)
+
+set TextTransform="%common%\Microsoft Shared\TextTemplating\10.0\texttransform.exe"
+if not exist %TextTransform% set TextTransform="%common%\Microsoft Shared\TextTemplating\1.2\texttransform.exe"
+if not exist %TextTransform% set TextTransform="%program%\MonoDevelop\AddIns\MonoDevelop.TextTemplating\TextTransform.exe"
+
 echo running texttransform..
 cd %~dp0
 
 
 cd ..\..\Sparklelib\windows
-"%CommonProgramFiles%\Microsoft Shared\TextTemplating\1.2\texttransform.exe" -out GlobalAssemblyInfo.cs GlobalAssemblyInfo.tt
+%TextTransform% -out GlobalAssemblyInfo.cs GlobalAssemblyInfo.tt
