3d9364209bab16a89f48690dd4f4394a52c23b4e
Niklas Angebrand <angebrand@liamg.moc>
2011-09-20 21:22:23 +0200

    Added MsysGit bundle to WiX installation

--- SparkleShare/Windows/build.cmd
+++ SparkleShare/Windows/build.cmd
@@ -4,12 +4,14 @@ set msbuild="%WinDirNet%\v3.5\msbuild.exe"
 if not exist %msbuild% set msbuild="%WinDirNet%\v4.0.30319\msbuild.exe"
 set wixBinDir=%WIX%\bin
 
-%msbuild% /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" %~dp0\SparkleShare.sln
+rem %msbuild% /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" %~dp0\SparkleShare.sln
 
 if "%1"=="installer" (
 	if exist "%wixBinDir%" (
+		"%wixBinDir%\heat.exe" dir "%git_install_root%." -cg msysGitComponentGroup -gg -scom -sreg -sfrag -srd -dr MSYSGIT_DIR -t addmedia.xlst -var wix.msysgitpath -o msysgit.wxs
 		"%wixBinDir%\candle" "%~dp0\SparkleShare.wxs"
-		"%wixBinDir%\light" -ext WixUIExtension Sparkleshare.wixobj
+		"%wixBinDir%\candle" "msysgit.wxs
+		"%wixBinDir%\light" -ext WixUIExtension Sparkleshare.wixobj msysgit.wixobj -dmsysgitpath=%git_install_root% -o SparkleShare.msi
 		echo SparkleShare.msi created.
 
 	) else (
