54302d8d8f9ac7d08bc2a299f3ba879a9ea428a0
Hylke Bons <hylkebons@gmail.com>
2011-09-22 20:11:08 +0100

    git: filling empty dirs: don't bother removing the .empty

--- SparkleLib/Git/SparkleRepoGit.cs
+++ SparkleLib/Git/SparkleRepoGit.cs
@@ -522,12 +522,8 @@ namespace SparkleLib {
                 FillEmptyDirectories (child_path);
             }
 
-            if (Directory.GetFiles (path).Length == 0) {
+            if (Directory.GetFiles (path).Length == 0)
                 File.Create (Path.Combine (path, ".empty")).Close ();
-
-            } else if (File.Exists (Path.Combine (path, ".empty"))) {
-                File.Delete (Path.Combine (path, ".empty"));
-            }
         }
 
 
