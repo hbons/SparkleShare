ebebe5bfcbfb7b0410864b75eced84269c5d2911
Konstantinos Vaggelakos <kozze89@gmail.com>
2011-10-05 22:55:22 +0200

    Added the git command to get the TextTemplating repo, which is needed in order to successfully build the application

--- SparkleShare/Windows/build.cmd
+++ SparkleShare/Windows/build.cmd
@@ -2,5 +2,6 @@
 set WinDirNet=%WinDir%\Microsoft.NET\Framework
 set msbuild="%WinDirNet%\v3.5\msbuild.exe"
 if not exist %msbuild% set msbuild="%WinDirNet%\v4.0.30319\msbuild.exe"
+git submodule update --init
 
 %msbuild% /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" %~dp0\SparkleShare.sln
