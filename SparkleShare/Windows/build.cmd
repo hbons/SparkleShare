f715217ff086108e7b3dd1b63e6660e4358531ec
Konstantinos Vaggelakos <kozze89@gmail.com>
2011-10-06 00:29:51 +0200

    Updated the build.cmd to update the submodules to make a cleaner build process

--- SparkleShare/Windows/build.cmd
+++ SparkleShare/Windows/build.cmd
@@ -1,7 +1,15 @@
 @echo off
 set WinDirNet=%WinDir%\Microsoft.NET\Framework
 set msbuild="%WinDirNet%\v3.5\msbuild.exe"
+set gitpath="C:\msysgit\bin\git.exe"
 if not exist %msbuild% set msbuild="%WinDirNet%\v4.0.30319\msbuild.exe"
+if not exist %gitpath% (
+ echo "Could not find git binary at %gitpath%, please install msysgit to C:\msysgit"
+ pause
+ exit
+ )
+set PATH=C:\msysgit\bin;%PATH%
+cd ..\..\
 git submodule update --init
-
+cd SparkleShare\Windows
 %msbuild% /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" %~dp0\SparkleShare.sln
