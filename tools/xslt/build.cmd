a3a98e42c056d7df3f08d4990119745694a1a5c5
wimh <wimh@github>
2011-10-17 21:20:38 +0200

    fix errors after merge, add code to support new features

--- tools/xslt/build.cmd
+++ tools/xslt/build.cmd
@@ -1,9 +1,6 @@
 @echo off
-
-call %~dp0\..\..\data\plugins\build.cmd
-
 set WinDirNet=%WinDir%\Microsoft.NET\Framework
 set msbuild="%WinDirNet%\v3.5\msbuild.exe"
 if not exist %msbuild% set msbuild="%WinDirNet%\v4.0.30319\msbuild.exe"
 
-%msbuild% /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" %~dp0\SparkleShare.sln
+%msbuild% /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" %~dp0\xslt.sln
