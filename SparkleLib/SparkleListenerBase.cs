8d315b7fa95e409e43ddbde65770fb2759faa092
Hylke Bons <hylkebons@gmail.com>
2011-11-13 22:03:13 +0000

    listener: point to a DNS record instead of an IP address

--- SparkleLib/SparkleListenerBase.cs
+++ SparkleLib/SparkleListenerBase.cs
@@ -52,7 +52,7 @@ namespace SparkleLib {
                 // don't have your own. All data needed to connect is hashed and
                 // we don't store any personal information ever
 
-                uri = "tcp://204.62.14.135:1986"; // TODO: announcements.sparkleshare.org
+                uri = "tcp://notifications.sparkleshare.org:1986";
             }
 
             Uri announce_uri = new Uri (uri);
