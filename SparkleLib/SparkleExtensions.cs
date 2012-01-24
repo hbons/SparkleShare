a37a0410187998bd7771b73e573cb58ea1437dcd
praktikant <praktikant@mergsystems>
2012-01-24 10:33:30 +0100

    Fixing #298 again first try

---
+++ SparkleLib/SparkleExtensions.cs
@@ -0,0 +1,35 @@
+//   SparkleShare, a collaboration and sharing tool.
+//   Copyright (C) 2010  Hylke Bons (hylkebons@gmail.com)
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program. If not, see (http://www.gnu.org/licenses/).
+
+
+using System;
+using System.IO;
+
+namespace SparkleLib {
+
+    public static class Extensions {
+
+        public static string Combine (this String [] parts)
+        {
+            string new_path = "";
+
+            foreach (string part in parts)
+                new_path = Path.Combine (new_path, part);
+
+            return new_path;
+        }
+    }
+}
