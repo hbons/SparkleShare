f9462f095f42deae0f398c8512f3b5e527991565
serras <trupill@gmail.com>
2012-02-12 17:24:24 +0100

    Chnage slightly error message (closes #9)

--- SparkleShare/Windows/SparkleSetup.Designer.cs
+++ SparkleShare/Windows/SparkleSetup.Designer.cs
@@ -28,17 +28,17 @@ namespace SparkleShare {
             this.tabControl = new SparkleShare.controls.TablessControl();
             this.setupPage = new System.Windows.Forms.TabPage();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.label11 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
             this.EmailEntry = new System.Windows.Forms.TextBox();
             this.buttonNext = new System.Windows.Forms.Button();
             this.NameEntry = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
             this.addPage = new System.Windows.Forms.TabPage();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.panel_server_selection = new System.Windows.Forms.Panel();
             this.label4 = new System.Windows.Forms.Label();
+            this.panel_server_selection = new System.Windows.Forms.Panel();
             this.FolderEntry = new SparkleShare.controls.ExampleTextBox();
             this.label14 = new System.Windows.Forms.Label();
             this.ServerEntry = new SparkleShare.controls.ExampleTextBox();
@@ -56,16 +56,16 @@ namespace SparkleShare {
             this.errorPage = new System.Windows.Forms.TabPage();
             this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
             this.buttonTryAgain = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.finishedPage = new System.Windows.Forms.TabPage();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.buttonFinished = new System.Windows.Forms.Button();
-            this.buttonOpenFolder = new System.Windows.Forms.Button();
             this.tableLayoutPanel5 = new System.Windows.Forms.TableLayoutPanel();
             this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonOpenFolder = new System.Windows.Forms.Button();
+            this.buttonFinished = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.tabControl.SuspendLayout();
             this.setupPage.SuspendLayout();
@@ -144,29 +144,6 @@ namespace SparkleShare {
             this.tableLayoutPanel1.Size = new System.Drawing.Size(500, 364);
             this.tableLayoutPanel1.TabIndex = 8;
             // 
-            // label11
-            // 
-            this.tableLayoutPanel1.SetColumnSpan(this.label11, 2);
-            this.label11.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label11.Location = new System.Drawing.Point(8, 30);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(484, 30);
-            this.label11.TabIndex = 2;
-            this.label11.Text = "Before we can create a SparkleShare folder on this computer, we need a few bits o" +
-                "f information from you.";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.tableLayoutPanel1.SetColumnSpan(this.label10, 2);
-            this.label10.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(8, 5);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(484, 25);
-            this.label10.TabIndex = 1;
-            this.label10.Text = "Welcome to SparkleShare!";
-            // 
             // EmailEntry
             // 
             this.EmailEntry.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -186,7 +163,7 @@ namespace SparkleShare {
             this.buttonNext.TabIndex = 3;
             this.buttonNext.Text = "Next";
             this.buttonNext.UseVisualStyleBackColor = true;
-			this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
             // 
             // NameEntry
             // 
@@ -196,6 +173,18 @@ namespace SparkleShare {
             this.NameEntry.Size = new System.Drawing.Size(411, 20);
             this.NameEntry.TabIndex = 9;
             this.NameEntry.TextChanged += new System.EventHandler(this.CheckSetupPage);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.label10, 2);
+            this.label10.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(8, 5);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(484, 25);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "Welcome to SparkleShare!";
             // 
             // label13
             // 
@@ -208,6 +197,17 @@ namespace SparkleShare {
             this.label13.TabIndex = 8;
             this.label13.Text = "Email:";
             this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label11
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.label11, 2);
+            this.label11.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label11.Location = new System.Drawing.Point(8, 30);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(484, 30);
+            this.label11.TabIndex = 2;
+            this.label11.Text = "Before we can create a SparkleShare folder on this computer, we need a few bits o" +
+                "f information from you.";
             // 
             // label12
             // 
@@ -260,15 +260,6 @@ namespace SparkleShare {
             this.tableLayoutPanel2.Size = new System.Drawing.Size(500, 364);
             this.tableLayoutPanel2.TabIndex = 18;
             // 
-            // panel_server_selection
-            // 
-            this.tableLayoutPanel2.SetColumnSpan(this.panel_server_selection, 2);
-            this.panel_server_selection.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel_server_selection.Location = new System.Drawing.Point(13, 63);
-            this.panel_server_selection.Name = "panel_server_selection";
-            this.panel_server_selection.Size = new System.Drawing.Size(474, 171);
-            this.panel_server_selection.TabIndex = 12;
-            // 
             // label4
             // 
             this.label4.AutoSize = true;
@@ -280,6 +271,15 @@ namespace SparkleShare {
             this.label4.Size = new System.Drawing.Size(474, 30);
             this.label4.TabIndex = 0;
             this.label4.Text = "Where\'s your project hosted?";
+            // 
+            // panel_server_selection
+            // 
+            this.tableLayoutPanel2.SetColumnSpan(this.panel_server_selection, 2);
+            this.panel_server_selection.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_server_selection.Location = new System.Drawing.Point(13, 63);
+            this.panel_server_selection.Name = "panel_server_selection";
+            this.panel_server_selection.Size = new System.Drawing.Size(474, 171);
+            this.panel_server_selection.TabIndex = 12;
             // 
             // FolderEntry
             // 
@@ -483,15 +483,6 @@ namespace SparkleShare {
             this.buttonTryAgain.UseVisualStyleBackColor = true;
             this.buttonTryAgain.Click += new System.EventHandler(this.buttonTryAgain_Click);
             // 
-            // label3
-            // 
-            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label3.Location = new System.Drawing.Point(13, 108);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(480, 70);
-            this.label3.TabIndex = 2;
-            this.label3.Text = resources.GetString("label3.Text");
-            // 
             // label2
             // 
             this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -502,6 +493,15 @@ namespace SparkleShare {
             this.label2.Text = "We don\'t know exactly what the problem is, but we can try to help you pinpoint it" +
                 ".";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(13, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(480, 70);
+            this.label3.TabIndex = 2;
+            this.label3.Text = resources.GetString("label3.Text");
             // 
             // label1
             // 
@@ -523,44 +523,6 @@ namespace SparkleShare {
             this.finishedPage.TabIndex = 4;
             this.finishedPage.Text = "Finished";
             this.finishedPage.UseVisualStyleBackColor = true;
-            // 
-            // label9
-            // 
-            this.label9.Location = new System.Drawing.Point(13, 35);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(307, 15);
-            this.label9.TabIndex = 3;
-            this.label9.Text = "Access the files from your SparkleShare folder.";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
-            this.label8.Location = new System.Drawing.Point(13, 10);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(227, 20);
-            this.label8.TabIndex = 2;
-            this.label8.Text = "Project successfully added!";
-            // 
-            // buttonFinished
-            // 
-            this.buttonFinished.Location = new System.Drawing.Point(84, 3);
-            this.buttonFinished.Name = "buttonFinished";
-            this.buttonFinished.Size = new System.Drawing.Size(75, 23);
-            this.buttonFinished.TabIndex = 1;
-            this.buttonFinished.Text = "Finish";
-            this.buttonFinished.UseVisualStyleBackColor = true;
-            this.buttonFinished.Click += new System.EventHandler(this.buttonFinished_Click);
-            // 
-            // buttonOpenFolder
-            // 
-            this.buttonOpenFolder.Location = new System.Drawing.Point(3, 3);
-            this.buttonOpenFolder.Name = "buttonOpenFolder";
-            this.buttonOpenFolder.Size = new System.Drawing.Size(75, 23);
-            this.buttonOpenFolder.TabIndex = 0;
-            this.buttonOpenFolder.Text = "Open Folder";
-            this.buttonOpenFolder.UseVisualStyleBackColor = true;
-            this.buttonOpenFolder.Click += new System.EventHandler(this.buttonOpenFolder_Click);
             // 
             // tableLayoutPanel5
             // 
@@ -591,6 +553,44 @@ namespace SparkleShare {
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
             this.flowLayoutPanel2.Size = new System.Drawing.Size(162, 30);
             this.flowLayoutPanel2.TabIndex = 0;
+            // 
+            // buttonOpenFolder
+            // 
+            this.buttonOpenFolder.Location = new System.Drawing.Point(3, 3);
+            this.buttonOpenFolder.Name = "buttonOpenFolder";
+            this.buttonOpenFolder.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpenFolder.TabIndex = 0;
+            this.buttonOpenFolder.Text = "Open Folder";
+            this.buttonOpenFolder.UseVisualStyleBackColor = true;
+            this.buttonOpenFolder.Click += new System.EventHandler(this.buttonOpenFolder_Click);
+            // 
+            // buttonFinished
+            // 
+            this.buttonFinished.Location = new System.Drawing.Point(84, 3);
+            this.buttonFinished.Name = "buttonFinished";
+            this.buttonFinished.Size = new System.Drawing.Size(75, 23);
+            this.buttonFinished.TabIndex = 1;
+            this.buttonFinished.Text = "Finish";
+            this.buttonFinished.UseVisualStyleBackColor = true;
+            this.buttonFinished.Click += new System.EventHandler(this.buttonFinished_Click);
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(13, 35);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(307, 15);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "Access the files from your SparkleShare folder.";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label8.Location = new System.Drawing.Point(13, 10);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(227, 20);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Project successfully added!";
             // 
             // SparkleSetup
             // 
