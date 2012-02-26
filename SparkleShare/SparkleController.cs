05e1c53107adf5dc084def60305638e10670f1eb
Hylke Bons <hylkebons@gmail.com>
2012-02-26 22:11:25 +0000

    controller: remove unneeded references

--- SparkleShare/SparkleController.cs
+++ SparkleShare/SparkleController.cs
@@ -16,14 +16,9 @@
 
 
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading;
 
-using Mono.Unix;
 using SparkleLib;
 
 namespace SparkleShare {
