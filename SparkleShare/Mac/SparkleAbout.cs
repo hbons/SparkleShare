84413d2c533612b207e770ae61b11cd87f33d212
Hylke Bons <hylkebons@gmail.com>
2012-03-01 20:46:25 +0000

    mac: Fix more memory warnings

--- SparkleShare/Mac/SparkleAbout.cs
+++ SparkleShare/Mac/SparkleAbout.cs
@@ -41,126 +41,147 @@ namespace SparkleShare {
 
         public SparkleAbout () : base ()
         {
-            SetFrame (new RectangleF (0, 0, 640, 281), true);
-            Center ();
+            using (var a = new NSAutoreleasePool ())
+            {
+                SetFrame (new RectangleF (0, 0, 640, 281), true);
+                Center ();
 
-            Delegate    = new SparkleAboutDelegate ();
-            StyleMask   = (NSWindowStyle.Closable | NSWindowStyle.Titled);
-            Title       = "About SparkleShare";
-            MaxSize     = new SizeF (640, 281);
-            MinSize     = new SizeF (640, 281);
-            HasShadow   = true;
-            BackingType = NSBackingStore.Buffered;
+                Delegate    = new SparkleAboutDelegate ();
+                StyleMask   = (NSWindowStyle.Closable | NSWindowStyle.Titled);
+                Title       = "About SparkleShare";
+                MaxSize     = new SizeF (640, 281);
+                MinSize     = new SizeF (640, 281);
+                HasShadow   = true;
+                BackingType = NSBackingStore.Buffered;
 
-            CreateAbout ();
+                CreateAbout ();
+            }
 
             Controller.HideWindowEvent += delegate {
-                InvokeOnMainThread (delegate {
-                    PerformClose (this);
-                });
+                using (var a = new NSAutoreleasePool ())
+                {
+                    InvokeOnMainThread (delegate {
+                        PerformClose (this);
+                    });
+                }
             };
 
             Controller.ShowWindowEvent += delegate {
-                InvokeOnMainThread (delegate {
-                    OrderFrontRegardless ();
-                });
+                using (var a = new NSAutoreleasePool ())
+                {
+                    InvokeOnMainThread (delegate {
+                        OrderFrontRegardless ();
+                    });
+                }
             };
 
             Controller.NewVersionEvent += delegate (string new_version) {
-                InvokeOnMainThread (delegate {
-                    UpdatesTextField.StringValue = "A newer version (" + new_version + ") is available!";
-                    UpdatesTextField.TextColor   =
-                        NSColor.FromCalibratedRgba (0.45f, 0.62f, 0.81f, 1.0f);
-                });
+                using (var a = new NSAutoreleasePool ())
+                {
+                    InvokeOnMainThread (delegate {
+                        UpdatesTextField.StringValue = "A newer version (" + new_version + ") is available!";
+                        UpdatesTextField.TextColor   =
+                            NSColor.FromCalibratedRgba (0.45f, 0.62f, 0.81f, 1.0f);
+                    });
+                }
             };
 
             Controller.VersionUpToDateEvent += delegate {
-                InvokeOnMainThread (delegate {
-                    UpdatesTextField.StringValue = "You are running the latest version.";
-                    UpdatesTextField.TextColor   =
-                        NSColor.FromCalibratedRgba (0.45f, 0.62f, 0.81f, 1.0f);
-                });
+                using (var a = new NSAutoreleasePool ())
+                {
+                    InvokeOnMainThread (delegate {
+                        UpdatesTextField.StringValue = "You are running the latest version.";
+                        UpdatesTextField.TextColor   =
+                            NSColor.FromCalibratedRgba (0.45f, 0.62f, 0.81f, 1.0f);
+                    });
+                }
             };
 
             Controller.CheckingForNewVersionEvent += delegate {
-                InvokeOnMainThread (delegate {
-                    UpdatesTextField.StringValue = "Checking for updates...";
-                    UpdatesTextField.TextColor   =
-                        NSColor.FromCalibratedRgba (0.45f, 0.62f, 0.81f, 1.0f); // Tango Sky Blue #1
-                });
+                using (var a = new NSAutoreleasePool ())
+                {
+                    InvokeOnMainThread (delegate {
+                        UpdatesTextField.StringValue = "Checking for updates...";
+                        UpdatesTextField.TextColor   =
+                            NSColor.FromCalibratedRgba (0.45f, 0.62f, 0.81f, 1.0f); // Tango Sky Blue #1
+                    });
+                }
             };
         }
 
 
         private void CreateAbout ()
         {
-            string about_image_path = Path.Combine (NSBundle.MainBundle.ResourcePath,
-                "Pixmaps", "about.png");
+            using (var a = new NSAutoreleasePool ())
+            {
+                string about_image_path = Path.Combine (NSBundle.MainBundle.ResourcePath,
+                    "Pixmaps", "about.png");
 
-            AboutImage = new NSImage (about_image_path) {
-                Size = new SizeF (640, 260)
-            };
+                AboutImage = new NSImage (about_image_path) {
+                    Size = new SizeF (640, 260)
+                };
 
-            AboutImageView = new NSImageView () {
-                Image = AboutImage,
-                Frame = new RectangleF (0, 0, 640, 260)
-            };
+                AboutImageView = new NSImageView () {
+                    Image = AboutImage,
+                    Frame = new RectangleF (0, 0, 640, 260)
+                };
 
 
-            VersionTextField = new NSTextField () {
-                StringValue     = "version " + Controller.RunningVersion,
-                Frame           = new RectangleF (295, 140, 318, 22),
-                BackgroundColor = NSColor.White,
-                Bordered        = false,
-                Editable        = false,
-                DrawsBackground = false,
-                TextColor       = NSColor.White,
-                Font            = NSFontManager.SharedFontManager.FontWithFamily
-                    ("Lucida Grande", NSFontTraitMask.Unbold, 0, 11)
-            };
+                VersionTextField = new NSTextField () {
+                    StringValue     = "version " + Controller.RunningVersion,
+                    Frame           = new RectangleF (295, 140, 318, 22),
+                    BackgroundColor = NSColor.White,
+                    Bordered        = false,
+                    Editable        = false,
+                    DrawsBackground = false,
+                    TextColor       = NSColor.White,
+                    Font            = NSFontManager.SharedFontManager.FontWithFamily
+                        ("Lucida Grande", NSFontTraitMask.Unbold, 0, 11)
+                };
 
-            UpdatesTextField = new NSTextField () {
-                StringValue     = "Checking for updates...",
-                Frame           = new RectangleF (295, Frame.Height - 232, 318, 98),
-                Bordered        = false,
-                Editable        = false,
-                DrawsBackground = false,
-                Font            = NSFontManager.SharedFontManager.FontWithFamily
-                    ("Lucida Grande", NSFontTraitMask.Unbold, 0, 11),
-                TextColor       =
-                    NSColor.FromCalibratedRgba (0.45f, 0.62f, 0.81f, 1.0f) // Tango Sky Blue #1
-            };
+                UpdatesTextField = new NSTextField () {
+                    StringValue     = "Checking for updates...",
+                    Frame           = new RectangleF (295, Frame.Height - 232, 318, 98),
+                    Bordered        = false,
+                    Editable        = false,
+                    DrawsBackground = false,
+                    Font            = NSFontManager.SharedFontManager.FontWithFamily
+                        ("Lucida Grande", NSFontTraitMask.Unbold, 0, 11),
+                    TextColor       =
+                        NSColor.FromCalibratedRgba (0.45f, 0.62f, 0.81f, 1.0f) // Tango Sky Blue #1
+                };
 
-            CreditsTextField = new NSTextField () {
-                StringValue     = @"Copyright © 2010–" + DateTime.Now.Year + " Hylke Bons and others." +
-                                   "\n" +
-                                   "\n" +
-                                   "SparkleShare is Free and Open Source Software. You are free to use, modify, and redistribute it " +
-                                   "under the GNU General Public License version 3 or later.",
-                Frame           = new RectangleF (295, Frame.Height - 260, 318, 98),
-                TextColor       = NSColor.White,
-                DrawsBackground = false,
-                Bordered        = false,
-                Editable        = false,
-                Font            = NSFontManager.SharedFontManager.FontWithFamily
-                    ("Lucida Grande", NSFontTraitMask.Unbold, 0, 11),
-            };
+                CreditsTextField = new NSTextField () {
+                    StringValue     = @"Copyright © 2010–" + DateTime.Now.Year + " Hylke Bons and others." +
+                                       "\n" +
+                                       "\n" +
+                                       "SparkleShare is Free and Open Source Software. You are free to use, modify, and redistribute it " +
+                                       "under the GNU General Public License version 3 or later.",
+                    Frame           = new RectangleF (295, Frame.Height - 260, 318, 98),
+                    TextColor       = NSColor.White,
+                    DrawsBackground = false,
+                    Bordered        = false,
+                    Editable        = false,
+                    Font            = NSFontManager.SharedFontManager.FontWithFamily
+                        ("Lucida Grande", NSFontTraitMask.Unbold, 0, 11),
+                };
 
-//            WebsiteButton.Activated += delegate {
-//                NSUrl url = new NSUrl ("http://www.sparkleshare.org/");
-//                NSWorkspace.SharedWorkspace.OpenUrl (url);
-//            };
+    //            WebsiteButton.Activated += delegate {
+    //                NSUrl url = new NSUrl ("http://www.sparkleshare.org/");
+    //                NSWorkspace.SharedWorkspace.OpenUrl (url);
+    //            };
 
-//            CreditsButton.Activated += delegate {
-//                NSUrl url = new NSUrl ("http://www.sparkleshare.org/credits/");
-//                NSWorkspace.SharedWorkspace.OpenUrl (url);
-//            };
+    //            CreditsButton.Activated += delegate {
+    //                NSUrl url = new NSUrl ("http://www.sparkleshare.org/credits/");
+    //                NSWorkspace.SharedWorkspace.OpenUrl (url);
+    //            };
 
-            ContentView.AddSubview (AboutImageView);
+                ContentView.AddSubview (AboutImageView);
 
-            ContentView.AddSubview (VersionTextField);
-            ContentView.AddSubview (UpdatesTextField);
-            ContentView.AddSubview (CreditsTextField);
+                ContentView.AddSubview (VersionTextField);
+                ContentView.AddSubview (UpdatesTextField);
+                ContentView.AddSubview (CreditsTextField);
+            }
         }
 
 
