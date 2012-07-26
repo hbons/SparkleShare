e17cda8a38eabfc55c82af3d9a1a16035e1177fd
Hylke Bons <hylkebons@gmail.com>
2012-07-26 15:17:29 +0200

    build: move version number definition to central place, clean up build tools

--- SparkleShare/Windows/tools/TextTemplating/build.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-set WinDirNet=%WinDir%\Microsoft.NET\Framework
-set msbuild="%WinDirNet%\v4.0.30319\msbuild.exe"
-
-%msbuild% /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" "%~dp0\TextTemplating.sln"
--- SparkleShare/Windows/tools/xslt/build.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-set WinDirNet=%WinDir%\Microsoft.NET\Framework
-set msbuild="%WinDirNet%\v3.5\msbuild.exe"
-if not exist %msbuild% set msbuild="%WinDirNet%\v4.0.30319\msbuild.exe"
-
-%msbuild% /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" "%~dp0\xslt.sln"
