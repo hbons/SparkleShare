a22dc86d27fa422ff6482c38e9b0a455ad806f7d
Hylke Bons <hylkebons@gmail.com>
2012-08-07 16:43:37 +0200

    linux: Fix compile error

--- SparkleShare/Linux/SparkleUIHelpers.cs
+++ SparkleShare/Linux/SparkleUIHelpers.cs
@@ -45,7 +45,7 @@ namespace SparkleShare {
 
         public static Image GetImage (string name)
         {
-            string image_path = new string [] { Defines.INSTALL_DIR, "pixmaps", name };
+            string image_path = new string [] { Program.UI.AssetsPath, "pixmaps", name }.Combine ();
             return new Image (image_path);
         }
 
