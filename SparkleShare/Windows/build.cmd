b3350a50c1c78a9014ef2f38cf398ea0192c25bf
Friflaj <friflaj@redminebacklogs.net>
2012-08-17 15:58:13 +0200

    Auto-download msysgit for installer build

--- SparkleShare/Windows/build.cmd
+++ SparkleShare/Windows/build.cmd
@@ -1,11 +1,17 @@
 @echo off
 
-call %~dp0\..\Common\Plugins\build.cmd
+call "%~dp0\..\Common\Plugins\build.cmd"
 
 set WinDirNet=%WinDir%\Microsoft.NET\Framework
 set msbuild="%WinDirNet%\v4.0\msbuild.exe"
 if not exist %msbuild% set msbuild="%WinDirNet%\v4.0.30319\msbuild.exe"
 set wixBinDir=%WIX%\bin
+set binDir=%~dp0\..\..\bin
+set msysgitDir=%bindir%\msysgit
+set url2file=%~dp0\url2file.exe
+set curl=%~dp0\curl.exe
+set sevenzip=%~dp0\7za.exe
+set gitzip=%msysgitdir%\git.7z
 
 if not exist ..\..\bin mkdir ..\..\bin
 copy Pixmaps\sparkleshare-app.ico ..\..\bin\
@@ -13,6 +19,12 @@ copy Pixmaps\sparkleshare-app.ico ..\..\bin\
 %msbuild% /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" "%~dp0\SparkleShare.sln"
 
 if "%1"=="installer" (
+  if not exist "%msysgitDir%" (
+    echo Downloading msysgit
+    "%curl%" --progress-bar --create-dirs -o "%gitzip%"  http://msysgit.googlecode.com/files/PortableGit-1.7.11-preview20120710.7z
+    "%sevenzip%" x "%gitzip%" -o"%msysgitdir%"
+    del "%gitzip%"
+  )
 	if exist "%wixBinDir%" (
 	  if exist "%~dp0\SparkleShare.msi" del "%~dp0\SparkleShare.msi"
 		"%wixBinDir%\heat.exe" dir "%~dp0\..\..\bin\msysgit" -cg msysGitComponentGroup -gg -scom -sreg -sfrag -srd -dr MSYSGIT_DIR -var wix.msysgitpath -o msysgit.wxs
