7cf4b579d5129df7a62be4fba23adf5450df75f2
Hylke Bons <hylkebons@gmail.com>
2012-10-27 14:22:34 +0100

    setup: update phrasing

--- SparkleShare/Mac/SparkleSetup.cs
+++ SparkleShare/Mac/SparkleSetup.cs
@@ -900,7 +900,7 @@ namespace SparkleShare {
                     }
 
                     case 3: {
-                        Header      = "The status icon is here to help";
+                        Header      = "The status icon helps you";
                         Description = "It shows the syncing progress, provides easy access to " +
                             "your projects and let's you view recent changes.";
 
