1ed718d29e2d1e56030d57b98b5112df273e34c7
Hylke Bons <hylkebons@gmail.com>
2013-06-01 16:37:25 +0100

    build: Adjust version bump script to include Mac Info.plist. Closes #1297

--- build/bump-version.sh
+++ build/bump-version.sh
@@ -6,6 +6,9 @@ else
   sed -i.bak "s/ Version='[^']*'/ Version='$1'/" ../SparkleShare/Windows/SparkleShare.wxs
   sed -i.bak "s/assembly:AssemblyVersion *(\"[^\"]*\")/assembly:AssemblyVersion (\"$1\")/" ../SparkleLib/Defines.cs                 
   sed -i.bak "s/m4_define(.sparkleshare_version[^)]*)/m4_define([sparkleshare_version], [$1])/" ../configure.ac
+  cat ../SparkleShare/Mac/Info.plist | eval "sed -e '/<key>CFBundleShortVersionString<\/key>/{N;s#<string>.*<\/string>#<string>$1<\/string>#;}'" > ../SparkleShare/Mac/Info.plist.tmp
+  cat ../SparkleShare/Mac/Info.plist.tmp | eval "sed -e '/<key>CFBundleVersion<\/key>/{N;s#<string>.*<\/string>#<string>$1<\/string>#;}'" > ../SparkleShare/Mac/Info.plist
+  rm ../SparkleShare/Mac/Info.plist.tmp
   rm ../SparkleShare/Windows/SparkleShare.wxs.bak
   rm ../SparkleLib/Defines.cs.bak
   rm ../configure.ac.bak
