d66159938d4b3d9b28ee1d7b37febd9eb7c41e38
Hylke Bons <hylkebons@gmail.com>
2013-06-13 23:32:29 +0100

    Bump version and update news

--- SparkleLib/Defines.cs
+++ SparkleLib/Defines.cs
@@ -19,7 +19,7 @@ using System;
 using System.Reflection;
 
 [assembly:AssemblyTitle ("SparkleLib")]
-[assembly:AssemblyVersion ("1.1.0")]
+[assembly:AssemblyVersion ("1.2")]
 [assembly:AssemblyCopyright ("Copyright (c) 2010 Hylke Bons and others")]
 [assembly:AssemblyTrademark ("SparkleShare is a trademark of SparkleShare Ltd.")]
 
