1df81af7786827c097a8242c0bcb13277d64d281
Thanh Tung Nguyen <thngtong@gmail.com>
2014-04-13 11:28:32 +0100

    makefiles: Use a separate variable for tests link flags
    Signed-off-by: Thanh Tung Nguyen <thngtong@gmail.com>
    [rebased to the latest upstream code]
    Signed-off-by: Andrew Shadura <andrew.shadura@collabora.co.uk>

--- build/build.rules.mk
+++ build/build.rules.mk
@@ -19,7 +19,7 @@ ASSEMBLY_FILE = $(top_builddir)/bin/$(ASSEMBLY).$(ASSEMBLY_EXTENSION)
 INSTALL_DIR_RESOLVED = $(firstword $(subst , $(DEFAULT_INSTALL_DIR), $(INSTALL_DIR)))
 
 if ENABLE_TESTS
-    LINK = " $(NUNIT_LIBS)"
+    LINK_TESTS = "$(NUNIT_LIBS)"
     ENABLE_TESTS_FLAG = "-define:ENABLE_TESTS"
 endif
 
