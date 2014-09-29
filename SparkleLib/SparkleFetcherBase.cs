c3c35397a3f6e65ca84e674ed7fb4e1d3a964b7a
Hylke Bons <hylkebons@gmail.com>
2014-09-29 12:35:14 +0100

    Add MS Office '~$' temporary file exclude pattern. #1418

--- SparkleLib/SparkleFetcherBase.cs
+++ SparkleLib/SparkleFetcherBase.cs
@@ -95,6 +95,7 @@ namespace SparkleLib {
             "~*.ppt", "~*.PPT", "~*.pptx", "~*.PPTX",
             "~*.xls", "~*.XLS", "~*.xlsx", "~*.XLSX",
             "~*.doc", "~*.DOC", "~*.docx", "~*.DOCX",
+            "~$*",
             "*.a$v", // QuarkXPress
             "*/CVS/*", ".cvsignore", "*/.cvsignore", // CVS
             "/.svn/*", "*/.svn/*", // Subversion
