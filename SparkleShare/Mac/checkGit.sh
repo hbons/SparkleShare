eca1005415178a0bbb9072e8950245cfdf74c553
Jose A Mendoza <orien3243@users.noreply.github.com>
2016-07-17 15:06:09 -0400

    bug fix line 29 and 31
    I was working on a script of mine and I realized the way I wrote the test commands do not work. They need a space between the curly braces and a semicolon at the end in order to work.

--- SparkleShare/Mac/checkGit.sh
+++ SparkleShare/Mac/checkGit.sh
@@ -26,9 +26,9 @@ then
 
   curl -s https://www.kernel.org/pub/software/scm/git/git-${gitVersion}.tar.gz > git.tar.gz
 
-  test -e git.tar.gz || {echo "Failed to download git"; exit 1}
+  test -e git.tar.gz || { echo "Failed to download git"; exit 1; }
 
-  test "$file_hash" = "$list_hash" || {echo "SHA256 Mistmatch" ;exit 1}
+  test "$file_hash" = "$list_hash" || { echo "SHA256 Mistmatch"; exit 1; }
 
     tar xf git.tar.gz
     cd git-${gitVersion}
