36e7ebc6a450a0adf1b940aa70f4df478ff35b20
Hylke Bons <hi@planetpeanut.uk>
2017-07-23 13:47:54 +0100

    Change my email address

--- Sparkles/Configuration.cs
+++ Sparkles/Configuration.cs
@@ -1,5 +1,5 @@
 //   SparkleShare, a collaboration and sharing tool.
-//   Copyright (C) 2010  Hylke Bons <hylkebons@gmail.com>
+//   Copyright (C) 2010  Hylke Bons <hi@planetpeanut.uk>
 //
 //   This program is free software: you can redistribute it and/or modify
 //   it under the terms of the GNU Lesser General Public License as 
