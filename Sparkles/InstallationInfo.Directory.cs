55048e1f7aa9a12ca779f26a42fc62e4982df6a4
Hylke Bons <hi@planetpeanut.uk>
2017-12-17 11:31:04 +0100

    Bump version to 2.0.1

--- Sparkles/InstallationInfo.Directory.cs
+++ Sparkles/InstallationInfo.Directory.cs
@@ -18,7 +18,7 @@
 using System.Reflection;
 
 [assembly:AssemblyTitle ("Sparkles")]
-[assembly:AssemblyVersion ("2.0")]
+[assembly:AssemblyVersion ("2.0.1")]
 [assembly:AssemblyCopyright ("Copyright (c) 2010 Hylke Bons and others")]
 
 namespace Sparkles {
