b3f33789f3978e6b35f50a6fd52ab1896cc5e76c
Hylke Bons <hi@planetpeanut.uk>
2018-01-02 15:22:52 +0100

    build: Warn (but don't stop) on git download SHA256 mismatch. Closes #1701

--- SparkleShare/Mac/checkGit.sh
+++ SparkleShare/Mac/checkGit.sh
@@ -11,16 +11,28 @@ function abspath()
   esac
 }
 
+
+LINE=$(cat ${projectFolder}/git.download)
+TMP=()
+
+for val in $LINE ; do
+        TMP+=("$val")
+done
+
 export projectFolder=$(dirname $0)
 export projectFolder=$(abspath ${projectFolder})
-export gitDownload=$(cat ${projectFolder}/git.download)
+export gitDownload="${TMP[0]}"
+export gitSHA256="${TMP[1]}"
 
 set -e
+
 
 if [ ! -f ${projectFolder}/git.tar.gz ]
 then
   curl --silent --location ${gitDownload} > git.tar.gz
   test -e git.tar.gz || { echo "Failed to download git"; exit 1; }
+
+  printf "${gitSHA256}  git.tar.gz" | shasum --check --algorithm 256
 
   mkdir git/
   tar xzf git.tar.gz --directory git/
