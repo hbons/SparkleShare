2232d222597cbbf2920ecb9eec6c4a5b3f93729c
Hylke Bons <hi@planetpeanut.uk>
2018-02-20 00:31:25 +0000

    build: Fix some hardcoded paths

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 echo "-> gtk-update-icon-cache"
-gtk-update-icon-cache --quiet --force --ignore-theme-index /usr/share/icons/hicolor > /dev/null
+gtk-update-icon-cache --quiet --force --ignore-theme-index ${MESON_INSTALL_PREFIX}/share/icons/hicolor
 
 echo "-> update-desktop-database"
-update-desktop-database --quiet /usr/share/applications > /dev/null
+update-desktop-database --quiet ${MESON_INSTALL_PREFIX}/share/applications
 
