2db852e20206a08374e3f2d69a6ff622cb346329
Hylke Bons <hi@planetpeanut.uk>
2018-02-20 22:52:41 +0000

    Update build.environment.mk

--- build/build.environment.mk
+++ build/build.environment.mk
@@ -7,14 +7,14 @@ DIR_BIN = $(top_builddir)/bin
 
 # External libraries to link against, generated from configure
 LINK_SYSTEM = -r:System
-LINK_MONO_POSIX = -r:Mono.Posix
+LINK_MONO_REFS = -r:Mono.Posix -r:System.Xml.Linq
 
 LINK_GLIB = $(GLIB_SHARP_LIBS)
 LINK_GTK = $(GTK_SHARP_LIBS)
 LINK_WEBKIT = $(WEBKIT2_SHARP_LIBS)
 LINK_APP_INDICATOR = $(APP_INDICATOR_LIBS)
 
-REF_SPARKLES = $(LINK_SYSTEM) $(LINK_MONO_POSIX)
+REF_SPARKLES = $(LINK_SYSTEM) $(LINK_MONO_REFS)
 LINK_SPARKLES = -r:$(DIR_BIN)/Sparkles.dll -r:$(DIR_BIN)/Sparkles.Git.dll
 LINK_SPARKLES_DEPS = $(REF_SPARKLES) $(LINK_SPARKLES)
 
