0bcdaac563ca1c88a15ee3f3523dca16a97eb227
Hylke Bons <hi@planetpeanut.uk>
2018-06-18 20:30:11 +0100

    linux: Update status icons to match new app icon

--- SparkleShare/Linux/UserInterface.cs
+++ SparkleShare/Linux/UserInterface.cs
@@ -51,6 +51,9 @@ namespace SparkleShare
             application.Register (null);
             application.Activated += ApplicationActivatedDelegate;
 
+		foreach (string s in IconTheme.Default.SearchPath)
+            Console.WriteLine(s);
+
             if (IconTheme.Default != null)
                 IconTheme.Default.AppendSearchPath (Path.Combine (UserInterface.AssetsPath, "icons"));
 
