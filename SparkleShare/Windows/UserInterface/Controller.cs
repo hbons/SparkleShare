33c5c06601d568d4652edfaa76292bfb3760d5d2
uenz <uenz@gmx.net>
2020-06-20 22:09:38 +0200

    Changed path to use windows openssh ssh-keyscan.exe

--- SparkleShare/Windows/UserInterface/Controller.cs
+++ SparkleShare/Windows/UserInterface/Controller.cs
@@ -55,7 +55,7 @@ namespace SparkleShare {
             Environment.SetEnvironmentVariable ("HOME", Environment.GetFolderPath (Environment.SpecialFolder.UserProfile));
 
             SSHCommand.SSHPath = Path.Combine (msysgit_path, "usr", "bin");
-            SSHFetcher.SSHKeyScan = Path.Combine (msysgit_path, "usr", "bin", "ssh-keyscan.exe");
+            SSHFetcher.SSHKeyScan = "ssh-keyscan.exe";
             GitCommand.GitPath = Path.Combine (msysgit_path, "bin", "git.exe");
 
             base.Initialize ();
@@ -191,7 +191,7 @@ namespace SparkleShare {
         }
 
 
-        public override void Quit ()
+        public override void PlatformQuit ()
         {
             base.Quit ();
         }
